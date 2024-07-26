a3176959f59772916796d429057b18a822542f22
logukaruna <logukaruna28@gmail.com>
2024-07-26 19:32:56 +0530

    Modification of Folder Structures

--- Java/Excel.java
+++ Java/Excel.java
@@ -2,7 +2,7 @@ public class Excel {
     public static void main(String args[]) {
         String s = "A";
         int ascii = s.charAt(0);
-        int output = (ascii - 64)z;
+        int output = (ascii - 64);
         System.out.println(output);
         
     }
