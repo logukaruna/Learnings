1d40e62296802484b038efdb81258430a453ec24
logukaruna <logukaruna28@gmail.com>
2024-07-26 19:05:52 +0530

    New Files and New Learning of the Palindrome in sentence

---
+++ Java/Excel.java
@@ -0,0 +1,9 @@
+public class Excel {
+    public static void main(String args[]) {
+        String s = "A";
+        int ascii = s.charAt(0);
+        int output = (ascii - 64)z;
+        System.out.println(output);
+        
+    }
+}
--- Java/Hello.java
+++ Java/Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 
     public static void main(String[] args) {
         int n1, n2;
-        n1 = 23;
-        n2 = 43;
-        int n3 = n1 + n2;
+        n1 = 2;
+        n2 = 3;
+        int n3 = (n1 + n2)/2;
         System.out.print(n3);
     }
 }
