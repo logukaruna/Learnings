29b125209f11403546ee01aa005526d26f2fe8c4
logukaruna <logukaruna28@gmail.com>
2025-02-27 12:42:18 +0530

    Learned about Enum

---
+++ Java/enum_learn.java
@@ -0,0 +1,16 @@
+enum logu{
+
+    logu, Selva, Kishore;
+}
+
+
+
+public class enum_learn {
+    public static void main(String[] args) {
+        
+        logu l = logu.Kishore;
+        System.out.println(l.ordinal());
+    
+    }
+    
+}
